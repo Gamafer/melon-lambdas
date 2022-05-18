937f4664a87c9c022b66448b74b31c063d21d8e6
Fernando Garrote De La Macorra <43961985+Gamafer@users.noreply.github.com>
2022-05-18 11:43:15 +0200

    Add files via upload

---
+++ src/main/java/com/sinensia/lambdas/Main.java
@@ -0,0 +1,31 @@
+package com.sinensia.lambdas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Melon> melons = Arrays.asList(
+                new Melon("Gac", 5500, "Europe"),
+                new Melon("Bailan", 6000, "China"),
+                new Melon("Watermelon", 1200, "Europe"),
+                new Melon("Gac", 3400, "US"),
+                new Melon("Bailan", 1300, "China")
+        );
+
+        for (int i = 0; i < melons.size(); ++i) {
+            System.out.println(melons.get(i).toString());
+        }
+
+        List<Melon> bailan = Filters.filterByType(melons, "Bailan");
+        System.out.println("Lista filtrada: ");
+        for(Melon m: bailan) {
+            System.out.println(m);
+        }
+
+    }
+
+
+}
